795ec9a120ef67a0c021069912d60d21aab0563c
Daniil <doritosbag8@gmail.com>
2024-06-15 13:43:18 +0200

    testing php functionality + main page layout

---
+++ main_page/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='./main.css'/>
+    <title>Main page</title>
+</head>
+<body>
+    <h1 id='app-title'>EventManager</h1>
+    <h3 id='description'>Welcome to EventManager event management (duh) web-app! Sign up or log in </h3>    
+</body>
+</html>
